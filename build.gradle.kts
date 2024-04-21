1baa9e25538679bfeeea0bf44368ada942f48ed0
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2024-04-21 19:40:23 +0200

    Add FontIcon and JavaFx Dependecies on build.gralde

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,41 @@
 plugins {
+     // Apply the java plugin to add support for Java
     java
 }
 
-repositoires {
-    maveCentral()
+repositories {
+    mavenCentral()
 }
 
-dependecies {
-    implementation("org.openjfx:javafx:11")
-    implementation("mysql:mysql-connector-java:8.0.33")
+val javaFXModules = listOf(
+    "base",
+    "controls",
+    "fxml",
+    "swing",
+    "graphics"
+)
+
+val supportedPlatforms = listOf("linux", "mac", "win") // All required for OOP
+
+dependencies {
+    
+    // JavaFX: comment out if you do not need them
+    val javaFxVersion = 15
+    for (platform in supportedPlatforms) {
+        for (module in javaFXModules) {
+            implementation("org.openjfx:javafx-$module:$javaFxVersion:$platform")
+        }
+    }
+
+    // https://mvnrepository.com/artifact/org.webjars/font-awesome
     implementation("org.webjars:font-awesome:6.5.2")
+
+    //implementation ("de.jensd:fontawesomefx-fontawesome:4.7.0-11")
+
+   // https://mvnrepository.com/artifact/de.jensd/fontawesomefx
+    implementation("de.jensd:fontawesomefx:8.2")
+
+
 }
+
+
