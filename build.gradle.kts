d3ea0140e353622bb5af7abea818c193e2638cb3
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2024-04-21 03:43:53 +0200

    Add Dependecies on build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
     java
 }
+
+repositoires {
+    maveCentral()
+}
+
+dependecies {
+    implementation("org.openjfx:javafx:11")
+    implementation("mysql:mysql-connector-java:8.0.33")
+    implementation("org.webjars:font-awesome:6.5.2")
+}
